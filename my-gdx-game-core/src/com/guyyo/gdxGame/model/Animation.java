f9fb632bf1aef396fb40ce678a9580ebaffdb745
guyyo <owner@owner-HP>
2014-04-10 17:33:33 +0300

    a

--- my-gdx-game-core/src/com/guyyo/gdxGame/model/Animation.java
+++ my-gdx-game-core/src/com/guyyo/gdxGame/model/Animation.java
@@ -1,44 +1,57 @@
 package com.guyyo.gdxGame.model;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public abstract class Animation extends Actor {
-	
 	public static enum STATE {
 		SPAWN, ALIVE, DEAD
 	}
+
 	public STATE state;
-	TextureRegion[] frames;
-	float frameIndex, speed;
+	ArrayList<TextureRegion[]> frames = new ArrayList<TextureRegion[]>();
+	float frameCol, speed;
 
 	public abstract void draw(Batch batch, float alpha);
+
+	int frameRow;
 
 	public abstract void animate();
 
 	public void loadTexture(Texture t, int rows, int cols) {
 		TextureRegion[][] tmp = TextureRegion.split(t, t.getWidth() / cols,
 				t.getHeight() / rows);
-		frames = new TextureRegion[rows * cols ];
-		int index = 0;
+		frames.ensureCapacity(frameRow + rows);
 		for (int i = 0; i < rows; i++) {
+			frames.add(frameRow, new TextureRegion[cols]);
 			for (int j = 0; j < cols; j++) {
-				frames[index++] = tmp[i][j];
+				frames.get(frameRow)[j] = tmp[i][j];
 			}
+			frameRow++;
 		}
-		setOrigin(frames[0].getRegionWidth() / 2,
-				frames[0].getRegionHeight() / 2);
-		setWidth(frames[0].getRegionWidth());
-		setHeight(frames[0].getRegionHeight());
+	}
+
+	public void initParams() {
+		frameRow = 0;
+		setOrigin(frames.get(frameRow)[0].getRegionWidth() / 2,
+				frames.get(frameRow)[0].getRegionHeight() / 2);
+		setWidth(frames.get(frameRow)[0].getRegionWidth());
+		setHeight(frames.get(frameRow)[0].getRegionHeight());
+	}
+
+	public TextureRegion getFrame(int frameRow) {
+		return frames.get(frameRow)[(int) frameCol];
 	}
 
 	public float getSpeed() {
 		return speed;
 	}
-	
-	public void kill(){
+
+	public void kill() {
 		state = STATE.DEAD;
 	}
 }
